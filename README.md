9d30394eaae7fee9abcfa91608b242804aa926ae
James Sarasin <james.sarasin@gmail.com>
2021-05-14 19:24:47 -0600

    Sdf

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ Installing
 pip3 install SpeechRecognition
 pip3 install pyaudio
 pip3 install pocketsphinx
+sudo apt-get install portaudio19-dev python-all-dev python3-all-dev && sudo pip install pyaudio
+
+    python3 -m pip install pyautogui
 ```
--- typeit.py
+++ typeit.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-
+import pyaudio
 import speech_recognition as sr
+from pyautogui import press, typewrite, hotkey
+
 r = sr.Recognizer()
-mic = sr.Microphone()
+print("caat" + pyaudio.get_device_count())
 
+mic = sr.Microphone(0)
 
-with mic as source:
-    audio = r.listen(source)
-    print(r.recognize_sphinx(audio))
+while 1:
+    print("sdf");
+    with mic as source:
+        print("ct");
+        audio = r.listen(source)
+        typewrite(r.recognize_google(audio) + "\n\r")
+        # typewrite(r.recognize_sphinx(audio) + "\n\r")
